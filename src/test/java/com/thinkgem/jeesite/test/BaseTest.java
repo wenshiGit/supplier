bd4ed7f4eda88e60469609e38fed6a8514c3a436
ZiruiSong <1005952406@qq.com>
2019-04-08 16:06:36 +0800

    tese

--- src/test/java/com/thinkgem/jeesite/test/BaseTest.java
+++ src/test/java/com/thinkgem/jeesite/test/BaseTest.java
@@ -14,4 +14,5 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "classpath:spring-context.xml" })
 public class BaseTest {
+	//something
 }
